00c69a888c554cdd75456c34900afc0619e9b01e
gugan <gugan0279@gmail.com>
2026-02-02 20:20:55 +0530

    source code edited

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import time
 app_window = Tk() 
 app_window.title("Digital Clock") 
 app_window.geometry("420x150") 
-app_window.resizable(1,1)
+app_window.resizable(0,1)
 text_font= ("Boulder", 68, 'bold')
 background = "#f2e750"
 foreground= "#363529"
-border_width = 25
+border_width = 40
 
 label = Label(app_window, font=text_font, bg=background, fg=foreground, bd=border_width) 
 
